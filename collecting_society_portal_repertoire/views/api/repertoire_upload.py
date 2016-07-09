9923fdddcef77695315b2d2d4865eb44000cd3d9
Alexander Blum <alexander.blum@c3s.cc>
2016-07-10 00:22:28 +0200

    separates upload paths for complete/part/preview

--- collecting_society_portal_repertoire/views/api/repertoire_upload.py
+++ collecting_society_portal_repertoire/views/api/repertoire_upload.py
@@ -33,6 +33,9 @@ log = logging.getLogger(__name__)
 mime = magic.Magic(mime=True)
 
 _prefix = 'repertoire'
+_stage_part = 'part'
+_stage_complete = 'complete'
+_stage_preview = 'preview'
 
 
 # --- methods -----------------------------------------------------------------
@@ -64,26 +67,33 @@ def get_url(url, version, action, filename):
     return '/'.join([url, version, action, filename])
 
 
-def get_path(request, filename=None):
+def get_path(request, stage=_stage_complete, filename=None):
     base = request.registry.settings['api.c3supload.filepath']
     webuser = str(request.user.id)
     if filename:
-        return os.path.join(base, webuser, filename)
-    return os.path.join(base, webuser)
+        return os.path.join(base, webuser, stage, filename)
+    return os.path.join(base, webuser, stage)
 
 
-def get_info(request, file):
-    filename = os.path.basename(file)
-    uploaded = os.path.isfile(file)
-    resumable = os.path.isfile(file + '.part')
-    if resumable:
-        file += '.part'
-    if not (uploaded or resumable):
+def get_info(request, filename):
+
+    # get file paths
+    file_part = get_path(request, _stage_part, filename)
+    file_complete = get_path(request, _stage_complete, filename)
+
+    # get status
+    complete = os.path.isfile(file_complete)
+    resumable = not complete and os.path.isfile(file_part)
+    file = file_part if resumable else file_complete
+
+    # file not found
+    if not (complete or resumable):
         return {
             'name': filename,
             'resumable': False,
             'error': _(u'File not found')
         }
+
     return {
         'name': filename,
         'resumable': resumable,
@@ -114,44 +124,67 @@ def create_path(path):
 
 
 def save_file(descriptor, file):
+
+    # check file
     if os.path.isfile(file):
         return False
+
+    # save file
     try:
         with open(file, 'w') as f:
             shutil.copyfileobj(descriptor, f)
     except IOError:
         raise HTTPInternalServerError
+
     return os.path.isfile(file)
 
 
-def save_chunk(descriptor, file, contentrange):
-    chunkfile = file + ".part"
+def save_chunk(descriptor, file, tmpfile, contentrange):
+
     start, stop, length = contentrange
+
+    # check file
+    if os.path.isfile(file):
+        return False
+
+    # check chunk range
     size = 0
-    if os.path.isfile(chunkfile):
-        size = os.path.getsize(chunkfile)
+    if os.path.isfile(tmpfile):
+        size = os.path.getsize(tmpfile)
     if start != size:
         return False
+
+    # save chunk
     try:
-        with open(chunkfile, 'a') as f:
+        with open(tmpfile, 'a') as f:
             shutil.copyfileobj(descriptor, f)
     except IOError:
         raise HTTPInternalServerError
-    if os.path.getsize(chunkfile) == length:
+
+    # move tmpfile to file after last chunk
+    if os.path.getsize(tmpfile) == length:
         try:
-            shutil.copyfile(chunkfile, file)
-            os.remove(chunkfile)
+            shutil.copyfile(tmpfile, file)
+            os.remove(tmpfile)
             return (os.path.getsize(file) == length)
         except IOError:
             raise HTTPInternalServerError
-    return (os.path.getsize(chunkfile) == stop)
+
+    return (os.path.getsize(tmpfile) == stop)
 
 
 def delete_file(file):
+
+    # check file
+    if not os.path.isfile(file):
+        return True
+
+    # delete file
     try:
         os.remove(file)
     except IOError:
         pass
+
     return not os.path.isfile(file)
 
 
@@ -166,14 +199,6 @@ repertoire_upload = Service(
 )
 
 
-class UploadSchema(MappingSchema):
-    echo = SchemaNode(
-        String(),
-        location="body",
-        type='str'
-    )
-
-
 @repertoire_upload.options()
 def options_repertoire_upload(request):
     response = request.response
@@ -185,22 +210,24 @@ def options_repertoire_upload(request):
     validators=(authenticate))
 def post_repertoire_upload(request):
 
-    # create path
-    path = get_path(request)
-    if not os.path.exists(path):
-        create_path(path)
+    # create paths
+    for stage in [_stage_part, _stage_complete, _stage_preview]:
+        path = get_path(request, stage)
+        if not os.path.exists(path):
+            create_path(path)
 
     # upload files
     files = []
     for name, fieldStorage in request.POST.items():
         if not isinstance(fieldStorage, FieldStorage):
             continue
-
         filename = os.path.basename(fieldStorage.filename)
-        file = get_path(request, filename)
+        file_part = get_path(request, _stage_part, filename)
+        file_complete = get_path(request, _stage_complete, filename)
 
-        if os.path.isfile(file):
-            info = get_info(request, file)
+        # file exists
+        if os.path.isfile(file_complete):
+            info = get_info(request, filename)
             info.update({'error': _(u'File already exists.')})
             files.append(info)
             continue
@@ -212,7 +239,8 @@ def post_repertoire_upload(request):
         if contentrange:
             ok = save_chunk(
                 descriptor=fieldStorage.file,
-                file=file,
+                file=file_complete,
+                tmpfile=file_part,
                 contentrange=contentrange
             )
             if not ok:
@@ -222,12 +250,12 @@ def post_repertoire_upload(request):
         else:
             ok = save_file(
                 descriptor=fieldStorage.file,
-                file=file
+                file=file_complete
             )
             if not ok:
                 raise HTTPInternalServerError
 
-        info = get_info(request, file)
+        info = get_info(request, filename)
         files.append(info)
     return {'files': files}
 
@@ -252,14 +280,11 @@ def options_repertoire_list(request):
     validators=(authenticate))
 def get_repertoire_list(request):
     files = []
-    uploadpath = get_path(request)
-    if not os.path.isdir(uploadpath):
+    path_complete = get_path(request, _stage_complete)
+    if not os.path.isdir(path_complete):
         return {'files': []}
-    for filename in os.listdir(uploadpath):
-        if filename.endswith('.part'):
-            continue
-        file = get_path(request, filename)
-        info = get_info(request, file)
+    for filename in os.listdir(path_complete):
+        info = get_info(request, filename)
         if info:
             files.append(info)
     return {'files': files}
@@ -285,8 +310,7 @@ def options_repertoire_show(request):
     validators=(authenticate))
 def get_repertoire_show(request):
     filename = request.matchdict['filename']
-    file = get_path(request, filename)
-    return get_info(request, file)
+    return get_info(request, filename)
 
 
 # --- service: preview --------------------------------------------------------
@@ -309,7 +333,7 @@ def options_repertoire_preview(request):
     validators=(authenticate))
 def get_repertoire_preview(request):
     filename = request.matchdict['filename']
-    file = get_path(request, filename)
+    file = get_path(request, _stage_complete, filename)
     if not os.path.isfile(file):
         raise HTTPNotFound()
     return FileResponse(
@@ -339,8 +363,9 @@ def options_repertoire_delete(request):
     validators=(authenticate))
 def get_repertoire_delete(request):
     filename = request.matchdict['filename']
-    file = get_path(request, filename)
-    ok = delete_file(file)
-    if not ok:
-        raise HTTPInternalServerError
+    for stage in [_stage_part, _stage_complete, _stage_preview]:
+        file = get_path(request, stage, filename)
+        ok = delete_file(file)
+        if not ok:
+            raise HTTPInternalServerError
     return
