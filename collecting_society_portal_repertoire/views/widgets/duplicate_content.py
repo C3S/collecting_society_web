0bd7916ba2fbf37cd43cb721b3eef62e631cf7ae
sarah <sarah@ubook.local>
2018-02-17 04:07:48 +0100

    test widget output

--- collecting_society_portal_repertoire/views/widgets/duplicate_content.py
+++ collecting_society_portal_repertoire/views/widgets/duplicate_content.py
@@ -9,13 +9,14 @@ from ...services import _
 class DuplicateContentWidget():
 
     def __init__(self):
-        heading = _(u'Duplicates: ' + self.duplicate_content_count(request.user))
-        body = render(
-            '../../templates/widgets/duplicate_content.pt',
-            {'news': request.context.registry['content']['duplicate_content']},
-            request=request
-        )
-        return {'heading': heading, 'body': body}
+        return self.duplicate_content_count(request.user)
+        #heading = _(u'Duplicates')
+        #body = render(
+        #    '../../templates/widgets/duplicate_content.pt',
+        #    {'duplicate_content': request.context.registry['content']['duplicate_content']},
+        #    request=request
+        #)
+        #return {'heading': heading, 'body': body}
 
     def duplicate_content_count(user):
         return Content.search_duplicates_by_user(user.id)
