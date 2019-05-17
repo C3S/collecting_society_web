de1bcbb17a7fad8af8e80d308b635a897c437524
Thomas Mielke <mielket@gmx.de>
2019-05-17 16:48:03 +0200

    Welcome now translated as 'Hallo' in German to be gender-agnostic; needstest #797

--- po.sh
+++ po.sh
@@ -2,3 +2,4 @@ pot-create -o collecting_society_portal_repertoire/locale/collecting_society_por
 msgmerge --update collecting_society_portal_repertoire/locale/*/LC_MESSAGES/collecting_society_portal_repertoire.po collecting_society_portal_repertoire/locale/collecting_society_portal_repertoire.pot
 poedit collecting_society_portal_repertoire/locale/de/LC_MESSAGES/collecting_society_portal_repertoire.po
 msgfmt -o collecting_society_portal_repertoire/locale/de/LC_MESSAGES/collecting_society_portal_repertoire.mo collecting_society_portal_repertoire/locale/de/LC_MESSAGES/collecting_society_portal_repertoire.po
+echo "You might want to restart the portal container now in order to see the new translations."
