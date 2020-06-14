80f3cac9c66c0040044db0d5b8dfa100c279db44
Thomas Mielke <thomas.mielke@c3s.cc>
2020-06-14 18:38:53 +0200

    finished AddLocation except Spaces

--- collecting_society_web/models/location.py
+++ collecting_society_web/models/location.py
@@ -18,3 +18,36 @@ class Location(Tdb, MixinSearchById, MixinSearchByOid, MixinSearchAll):
     Model wrapper for Tryton model object 'location'
     """
     __name__ = 'location'
+
+    @classmethod
+    def create(cls, vlist):
+        """
+        Creates location
+
+        Args:
+          vlist (list): list of dicts with attributes to create publisher::
+
+            [
+                {
+                    'name': str (required)
+                },
+                {
+                    ...
+                }
+            ]
+
+        Raises:
+          KeyError: if required field is missing
+
+        Returns:
+          list: created location
+          None: if no object was created
+        """
+        log.debug('create location:\n{}'.format(vlist))
+        for values in vlist:
+            if 'name' not in values:
+                raise KeyError('name is missing')
+            if 'longitude' not in values or 'latitude' not in values:
+                raise KeyError('Full latitude/longitude geoinfo is necessary')
+        result = cls.get().create(vlist)
+        return result or None
--- collecting_society_web/views/forms/add_location.py
+++ collecting_society_web/views/forms/add_location.py
@@ -5,7 +5,7 @@ import logging
 import colander
 import deform
 
-from portal_web.models import Tdb, Party
+from portal_web.models import Tdb, Party, Address
 from portal_web.views.forms import FormController
 
 from ...services import _
@@ -39,17 +39,21 @@ class AddLocation(FormController):
 
         # generate vlist
         location = {
-            'name': self.appstruct['name'],
-            'category': self.appstruct['category'],
+            'name': self.appstruct['general']['name'],
+            'category': self.appstruct['general']['category'],
             # 'party': self.appstruct['party'],
-            'public': self.appstruct['public'],
-            'latitude': self.appstruct['latitude'],
-            'longitude': self.appstruct['longitude'],
+            'public': self.appstruct['general']['public'],
+            'latitude': self.appstruct['general']['latitude'],
+            'longitude': self.appstruct['general']['longitude'],
             # 'spaces': self.appstruct['spaces']
         }
 
         party = {
-            'name': self.appstruct['name'],
+            'name': self.appstruct['contact']['contact_name'],
+            'first_name': self.appstruct['contact']['contact_first_name'],
+            'website': self.appstruct['contact']['website'],
+            'email': self.appstruct['contact']['email'],
+            'fax': self.appstruct['contact']['fax'],
             # 'web_user': web_user,
             # member_c3s
             # member_c3s_token
@@ -63,17 +67,36 @@ class AddLocation(FormController):
             # identifiers
             # legal_person
             'common_public_interest': 'no'
+        }  
+
+        address = {
+            'name': self.appstruct['address']['address_name'],
+            'street': self.appstruct['address']['street'],
+            'streetbis': self.appstruct['address']['streetbis'],
+            'zip': self.appstruct['address']['zip'],
+            'city': self.appstruct['address']['city'],
+            'country': self.appstruct['address']['country']
+            # TODO: subsection
         }
 
         # remove empty fields
-        for index, value in location.items():
-            if not value:
-                del location[index]
+        for tab in [location, party, address]:
+            for index, value in tab.items():
+                if not value:
+                    del tab[index]
 
         # create location
+        if "name" not in party:
+            party["name"] = self.appstruct['general']['name']
         party_record = Party.create([party])
         if party_record:
             location["party"] = party_record[0].id  # party is a required field
+            if address != []:
+                address["party"] = party_record[0].id
+                if "name" not in address:
+                    address["name"] = self.appstruct['general']['name']
+                # address_record =
+                Address.create([address])
         location_record = Location.create([location])
 
         # user feedback
@@ -117,6 +140,8 @@ def deferred_category_widget(node, kw):
 
 # --- Fields ------------------------------------------------------------------
 
+# -- General tab --
+
 
 class NameField(colander.SchemaNode):
     oid = "name"
@@ -132,27 +157,130 @@ class CategoryField(colander.SchemaNode):
 class PublicField(colander.SchemaNode):
     oid = "public"
     schema_type = colander.Boolean
+    missing = ""
 
 
 class LatitudeField(colander.SchemaNode):
     oid = "latitude"
     schema_type = colander.Float
+    validator = colander.Range(min=-90, max=90)
 
 
 class LongitudeField(colander.SchemaNode):
     oid = "longitude"
     schema_type = colander.Float
+    validator = colander.Range(min=-90, max=90)
+
+
+# -- Contact info tab --
+
+
+class ContactNameField(colander.SchemaNode):
+    oid = "contact_name"
+    schema_type = colander.String
+    missing = ""
+
+
+class ContactFirstNameField(colander.SchemaNode):
+    oid = "contact_first_name"
+    schema_type = colander.String
+    missing = ""
+
+
+class WebsiteField(colander.SchemaNode):
+    oid = "website"
+    schema_type = colander.String
+    missing = ""
+
+
+class EmailField(colander.SchemaNode):
+    oid = "email"
+    schema_type = colander.String
+    missing = ""
+
+
+class FaxField(colander.SchemaNode):
+    oid = "fax"
+    schema_type = colander.String
+    missing = ""
+
+
+# -- Address tab --
+
+
+class AddressNameField(colander.SchemaNode):
+    oid = "address_name"
+    schema_type = colander.String
+    missing = ""
+
+
+class StreetNameField(colander.SchemaNode):
+    oid = "street"
+    schema_type = colander.String
+    missing = ""
+
+
+class StreetBisNameField(colander.SchemaNode):
+    oid = "streetbis"
+    schema_type = colander.String
+    missing = ""
+
+
+class ZipField(colander.SchemaNode):
+    oid = "zip"
+    schema_type = colander.String
+    missing = ""
+
+
+class CityField(colander.SchemaNode):
+    oid = "city"
+    schema_type = colander.String
+    missing = ""
+
+
+class CountryField(colander.SchemaNode):
+    oid = "country"
+    schema_type = colander.String
+    missing = ""
 
 
 # --- Schemas -----------------------------------------------------------------
 
-class AddLocationSchema(colander.Schema):
+
+class GeneralSchema(colander.Schema):
+    widget = deform.widget.MappingWidget(template='navs/mapping')
     name = NameField(title=_(u"Name"))
     category = CategoryField(title=_(u"Category"))
     public = PublicField(title=_(u"Public"))
     latitude = LatitudeField(title=_(u"Latitude"))
     longitude = LongitudeField(title=_(u"Longitude"))
-    # styles = StyleCheckboxField(title=_(u"Styles"))
+
+
+class ContactSchema(colander.Schema):
+    widget = deform.widget.MappingWidget(template='navs/mapping')
+    contact_name = ContactNameField(title=_(u"Contact person name"))
+    contact_first_name = ContactNameField(title=_(u"Contact person first name"))
+    website = WebsiteField(title=_(u"Website"))
+    email = EmailField(title=_(u"Email"))
+    fax = FaxField(title=_(u"Fax"))
+
+
+class AddressSchema(colander.Schema):
+    widget = deform.widget.MappingWidget(template='navs/mapping')
+    address_name = AddressNameField(title=_(u"Name (doorbell)"))
+    street = StreetNameField(title=_(u"Street"))
+    streetbis = StreetBisNameField(title=_(u"Street (bis)"))
+    zip = ZipField(title=_(u"Zip"))
+    city = CityField(title=_(u"City"))
+    country = CountryField(title=_(u"Country"))
+    # subdivision
+
+
+class AddLocationSchema(colander.Schema):
+    widget = deform.widget.FormWidget(template='navs/form', navstyle='tabs')
+    general = GeneralSchema(title=_(u"General"))
+    contact = ContactSchema(title=_(u"Contact information"))
+    address = AddressSchema(title=_(u"Business address"))
 
 
 # --- Forms -------------------------------------------------------------------
