f8c87ffabff68e81db92a57de1429f1bb7dd9aaf
Thomas Mielke <thomas.mielke@c3s.cc>
2023-04-04 02:16:07 +0200

    started Artist integration test

---
+++ collecting_society_web/tests/integration/test_artist.py
@@ -0,0 +1,62 @@
+# For copyright and license terms, see COPYRIGHT.rst (top level of repository)
+# Repository: https://github.com/C3S/collecting_society_web
+
+import pytest
+
+from selenium.webdriver.common.by import By
+
+from portal_web.tests.integration.pageobjects import DeformFormObject
+from portal_web.views.forms.login_web_user import login_form
+from portal_web.models import Tdb
+
+from ...views.forms.add_artist import (
+    add_artist_form
+)
+
+
+@pytest.fixture(autouse=True, scope='class')
+def web_user(create_web_user):
+    create_web_user(
+        email='licenser@username.test',
+        password='password',
+        opt_in_state='opted-in',
+    )
+
+
+class TestArtist:
+    """
+    Artist management scenario.
+    """
+
+    def test_010_login_licenser(self, browser):
+        """
+        login licenser
+        """
+        browser.get("/")
+        formid = 'LoginWebuser'
+        form = DeformFormObject(browser, login_form(), formid)
+        form.login_email.set('licenser@username.test')
+        form.login_password.set('password')
+        form.submit()
+        assert browser.find_elements(By.CLASS_NAME, 'cs-backend')
+        
+    @Tdb.transaction()
+    def test_020_create_artist(self, browser, request_with_registry):
+        """
+        add an artist
+        """
+        browser.get("/repertoire/artists/add")
+        browser.screenshot("navigated_to_artist_add")
+
+        formid = 'AddArtist'
+        form = DeformFormObject(
+            browser, add_artist_form(request_with_registry), formid)
+        form.group.set(False)
+        form.name.set('Testartist #12345')
+        # form.ipn_code.set('1970-01-01')
+        # form.register_email.set('a@webuser.test')
+        # form.register_password.set('awebuser')
+        # form.terms_accepted.set(True)
+        # form.register_webuser()
+        form.submit()
+        assert browser.find_element(By.CLASS_NAME, "alert-success")
