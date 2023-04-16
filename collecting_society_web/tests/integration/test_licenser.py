9f8d9715d4c663a4e52cb7e3ebcf6b14a551b83f
Thomas Mielke <thomas.mielke@c3s.cc>
2023-04-16 17:06:18 +0200

    added integration test for group artist

--- collecting_society_web/tests/integration/test_licenser.py
+++ collecting_society_web/tests/integration/test_licenser.py
@@ -52,25 +52,24 @@ class TestLicenser:
         form.submit()
         assert browser.find_elements(By.CLASS_NAME, 'cs-backend')
 
-    def test_012_navigate_to_repertoire(self, browser):
+    def test_020_navigate_to_repertoire(self, browser):
         browser.find_element(By.CLASS_NAME, "cs-menue-item-repertoire").click()
         assert browser.find_element(By.CLASS_NAME, "introtext")
 
-    def test_014_navigate_to_artist_list(self, browser):
+    def test_022_navigate_to_artist_list(self, browser):
         browser.find_element(By.CLASS_NAME, "cs-menue-item-artists").click()
         assert browser.current_url[-20:] == "/repertoire/artists/"
 
-    def test_016_click_add_artist(self, browser):
+    def test_024_click_add_artist(self, browser):
         browser.find_element(By.CLASS_NAME, "btn-artist-add").click()
+        # browser.get("/repertoire/artists/add")
         assert browser.current_url[-23:] == "/repertoire/artists/add"
         browser.screenshot("navigated_to_add_artist")
 
-    def test_020_create_artist(self, browser):
+    def test_026_create_artist(self, browser):
         """
-        add an artist
+        add a solo artist
         """
-        # browser.get("/repertoire/artists/add")
-
         formid = 'AddArtist'
         form = DeformFormObject(
             browser, add_artist_form, formid)
@@ -87,15 +86,15 @@ class TestLicenser:
         assert "This is example of a solo artist." in browser.page_source
         assert "12345678901" in browser.page_source  # IPN, and description
 
-    def test_025_click_edit_artist(self, browser):
+    def test_030_click_edit_artist(self, browser):
         browser.find_element(By.CLASS_NAME, "btn-artist-edit").click()
         assert "/artists/" in browser.current_url
         assert browser.current_url.endswith("/edit")
         browser.screenshot("navigated_to_edit_artist")
 
-    def test_030_edit_artist(self, browser):
+    def test_035_edit_artist(self, browser):
         """
-        edit an artist
+        edit first artist
         """
         formid = 'EditArtist'
         form = DeformFormObject(
@@ -104,7 +103,6 @@ class TestLicenser:
         form.ipn_code.set("98765432101")
         form.description.set("This is *an* example of a solo artist.")
         # TODO: test picture upload
-        # TODO: test group artist
         form.submit()
 
         assert browser.find_element(By.CLASS_NAME, "alert-success")
@@ -117,21 +115,101 @@ class TestLicenser:
         assert "This is *an* example of a solo artist." in browser.page_source
         assert "98765432101" in browser.page_source  # IPN, and description?
 
-    def test_040_navigate_to_creation(self, browser):
+    def test_040_navigate_to_artist_list(self, browser):
+        browser.find_element(By.CLASS_NAME, "cs-menue-item-artists").click()
+        assert browser.current_url[-20:] == "/repertoire/artists/"
+        browser.screenshot("navigated_to_artist_list")
+
+    def test_042_click_add_artist(self, browser):
+        browser.find_element(By.CLASS_NAME, "btn-artist-add").click()
+        # browser.get("/repertoire/artists/add")
+        assert browser.current_url[-23:] == "/repertoire/artists/add"
+        browser.screenshot("navigated_to_add_artist")
+
+    def test_044_create_artist(self, browser):
+        """
+        add another solo artist
+        """
+        formid = 'AddArtist'
+        form = DeformFormObject(
+            browser, add_artist_form, formid)
+        form.group.set(False)
+        form.name.set("Testartist #67890")
+        form.ipn_code.set("67890123451")
+        form.description.set("Another solo artist")
+        # TODO: test picture upload
+        form.submit()
+        assert browser.find_element(By.CLASS_NAME, "alert-success")
+
+        assert "Testartist #67890" in browser.page_source  # check for name,
+        assert "Another solo artist" in browser.page_source
+        assert "67890123451" in browser.page_source  # IPN, and description
+
+    def test_050_navigate_to_artist_list_again(self, browser):
+        browser.find_element(By.CLASS_NAME, "cs-menue-item-artists").click()
+        assert browser.current_url[-20:] == "/repertoire/artists/"
+
+    def test_052_click_add_artist_again(self, browser):
+        browser.find_element(By.CLASS_NAME, "btn-artist-add").click()
+        # browser.get("/repertoire/artists/add")
+        assert browser.current_url[-23:] == "/repertoire/artists/add"
+        browser.screenshot("navigated_to_add_artist")
+
+    def test_055_create_group_artist(self, browser):
+        """
+        add a group artist
+        """
+        formid = 'AddArtist'
+        form = DeformFormObject(
+            browser, add_artist_form, formid)
+        form.group.set(True)
+        form.name.set("Testartist #34567")
+        form.ipn_code.set("34567678901")
+        form.description.set("This is an example of a group artist.")
+        # TODO: test picture upload
+
+        # add first artist to group artist
+        browser.find_element(By.CLASS_NAME, "btn-sequence-add").click()
+        browser.screenshot("add_artist_to_group_clicked")
+        artist_add_buttons = browser.find_elements(
+            By.CLASS_NAME, "cs-datatables-btn-source-add")
+        import debugpy
+        debugpy.listen(("0.0.0.0", 52003))
+        debugpy.wait_for_client()
+        breakpoint()
+        assert len(artist_add_buttons) == 2  # expect exactly two artists
+        artist_add_buttons[0].click()
+        browser.screenshot("added_first_artist_to_group")
+
+        # add second artist to group artist
+        browser.find_element(By.CLASS_NAME, "btn-sequence-add").click()
+        artist_add_buttons = browser.find_elements(
+            By.CLASS_NAME, "cs-datatables-btn-source-add")
+        assert len(artist_add_buttons) == 1  # one remove and one add button
+        artist_add_buttons[0].click()
+        browser.screenshot("added_second_artist_to_group")
+
+        form.submit()
+        assert browser.find_element(By.CLASS_NAME, "alert-success")
+
+        assert "Testartist #34567" in browser.page_source  # check for name,
+        assert "This is an example of a group artist." in browser.page_source
+        assert "34567678901" in browser.page_source  # IPN, and description
+
+    def test_060_navigate_to_creations(self, browser):
         browser.find_element(By.CLASS_NAME, "cs-menue-item-creations").click()
         assert browser.current_url[-22:] == "/repertoire/creations/"
 
-    def test_042_click_add_creation(self, browser):
+    def test_062_click_add_creation(self, browser):
         browser.find_element(By.CLASS_NAME, "btn-creation-add").click()
+        # browser.get("/repertoire/creations/add")
         assert browser.current_url[-25:] == "/repertoire/creations/add"
         browser.screenshot("navigated_to_add_creation")
 
-    def XXXtest_050_create_creation(self, browser, request_with_registry):
+    def XXXtest_065_create_creation(self, browser, request_with_registry):
         """
         add an creation
         """
-        # browser.get("/repertoire/creations/add")
-
         formid = 'AddCreation'
         request_with_registry.webuser = WebUser.search_by_email(wu_email)
         form = DeformFormObject(
@@ -142,11 +220,11 @@ class TestLicenser:
         # form.submit()
         assert browser.find_element(By.CLASS_NAME, "alert-success")
 
-        # assert "Testcreation #12345" in browser.page_source  # check for name
+        assert "Testcreation #12345" in browser.page_source  # check for name
         # assert "example of a simple creation." in browser.page_source
         # assert "12345678901" in browser.page_source  # IPN, and description
 
-    def XXXtest_060_delete_creation(self, browser, request_with_registry):
+    def XXXtest_080_delete_creation(self, browser, request_with_registry):
         """
         delete an creation
         """
